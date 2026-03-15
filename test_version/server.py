565a4959267db974cf48e7d9089381a0b3ef636e
Lochen5511 <mcuser5511@gmail.com>
2026-03-15 21:34:12 +0800

    fsadasda

--- test_version/server.py
+++ test_version/server.py
@@ -317,7 +317,7 @@ if __name__ == '__main__':
     print("=" * 50)
     print("📡 本地網址: http://localhost:5000")
     print("💡 提示: 請使用 Localtunnel 建立公網隧道")
-    print("   指令: npx localtunnel --port 5000 --subdomain lochen5511")
+    print("   指令: npx localtunnel --port 5000 --subdomain lochen")
     print("=" * 50)
     
     # 啟動伺服器
